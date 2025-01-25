9a478aae823b1c49ba7d3c315858f9a6a59a52e9
graffybubby <124313651+graffybubby@users.noreply.github.com>
2025-01-25 18:13:30 -0500

    added module 2 case study file

--- MO2/MO2_CS.py
+++ MO2/MO2_CS.py
@@ -32,18 +32,20 @@ def collectValidate():
             # if the students last name is 'zzz' cancel further inputs by setting all validation bools to false
             # and stop the main loop by setting 'running' to false
             if studentLName.lower() == "zzz": 
-                print("Processing students now...")
+                print("\nProcessing students now...")
                 va = False
                 vb = False
                 vc = False
                 running = False
                 break
+            # confirm students last name contains only alphabetical characters
             elif studentLName.isalpha() == False:
                 print(studentNameError)
             else: break
         # collect student first name
         while(vb):
             studentFName = input("Please enter the students first name:")
+            # confirm students first name contains only alphabetical characters
             if studentFName.isalpha() == False:
                 print(studentNameError)
             else: vb = False
@@ -51,16 +53,67 @@ def collectValidate():
         while(vc):
             studentGpa = input("Please enter the students GPA:")
             # using previously defined function to validate that the students gpa is a float while
-            # avoiding using float(input(...)) so the console doesnt give an unsightly error message upon incorrect input type
-            if isFloat(studentGpa):
+            # avoiding using float(input(...) so the console doesnt give an unsightly error message upon incorrect input type
+            if isFloat(studentGpa) and studentGpa > 0:
                 vc = False
             else: print("Student GPA must maintain this structure '0.00'. Please try again.")
         # packing collected and validated inputs into a tuple 
         studentInfo = (studentLName, studentFName, float(studentGpa))
-        # if the students last name is 'zzz' do not append further data into the student list
+        # if the students last name is 'zzz' do not append further data into the students list
         if studentInfo[0] != "zzz":
-            # append valid student information into a list 
+            # append valid student information into the students list 
             students.append(studentInfo)
     return students  
       
-# function finding which students made dean's list or honor roll, accepts list of students
+# function finding which students made dean's list or honor roll, accepts list of students.
+def placement(students):
+    deans = []
+    honor = []
+    for student in students:
+        FName, LName, Gpa = student
+        # if a students gpa is 3.5 or greater, add them to the dean's list list
+        if Gpa >= 3.5:
+            deans.append(student)
+        # if a students gpa is less than 3.5 but greater than 3.25, add them to the honor roll
+        elif Gpa >= 3.25 and Gpa < 3.5:
+            honor.append(student)
+    # pack honor roll and dean's list lists into a tuple
+    hdcombo = (honor, deans)
+    return hdcombo
+
+# function displaying the students from honor roll and deans list, accepts tuple of lists (honor roll and dean's list).
+def displayData(hdcombo):
+    # unpack tuple containing the honor roll and dean's list
+    honor, deans = hdcombo
+    print("\nHonor roll students:")
+    # if the honor roll list contains any elements (students), display them.
+    if len(honor) != 0:
+        # iterate through honor roll list
+        for student in honor:
+            # unpack each student's tuple on honor roll
+            Lname, Fname, Gpa = student
+            print("Name: ", Fname, " ", Lname, " | GPA: ", Gpa)
+    else: print("    There are no students on the Honor roll.")
+
+    print("\nDean's list students:")
+    # if the dean's list contains any elements (students), display them.
+    if len(deans) != 0:
+        # iterate through the dean's list list
+        for student in deans:
+            # unpack each student's tuple on the dean's list
+            Lname, Fname, Gpa = student
+            print("Name: ", Fname, " ", Lname, " | GPA: ", Gpa)
+    else: print("    There are no students on the Dean's list.")
+
+# main function
+def main():
+    clear()
+    students = placement(collectValidate()) 
+    displayData(students)
+
+main()
+        
+
+    
+
+

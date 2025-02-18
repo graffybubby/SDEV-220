7ccaefd490e30cf0eb2bcb8020f07b2824b30ccd
graffybubby <124313651+graffybubby@users.noreply.github.com>
2025-02-17 19:46:28 -0500

    Uploaded test py file and dependencies from tutorial

---
+++ MO5/MO5_CA01/test.py
@@ -0,0 +1,28 @@
+import unittest
+
+from my_sum import sum
+from fractions import Fraction
+
+
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test that it can sum a list of integers
+        """
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
+    def test_list_fraction(self):
+        """
+        Test that it can sum a list of fractions
+        """
+        data = [Fraction(1, 4), Fraction(1, 4), Fraction(2, 5)]
+        result = sum(data)
+        self.assertEqual(result, 1)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+# the test_list_int function works as expected, but the test_list_fraction function failed with the following error: AssertionError: Fraction(1, 1) != 1 this is because the sum function doesn't support fractions.
